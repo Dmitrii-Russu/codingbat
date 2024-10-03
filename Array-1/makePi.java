58842692d92292adf0dbeefe2f45f190e3de3be9
Dmitrii-Russu <dmitriirussulink@gmail.com>
2024-10-03 15:17:36 +0300

    Create makePi.java
    Add makePi method to return the first three digits of pi
    
    - Implemented the makePi method that returns an integer array containing the digits {3, 1, 4}.
    - Added Javadoc comments to explain the method's purpose and return value.

---
+++ Array-1/makePi.java
@@ -0,0 +1,8 @@
+/**
+ * The makePi method returns an integer array containing the first three digits of pi.
+ *
+ * @return an integer array of length 3 containing the first three digits of pi: {3, 1, 4}.
+ */
+public int[] makePi() {
+    return new int[] {3, 1, 4}; // The digits of pi: 3, 1, and 4
+}
