6ea8e1098f68259f7aa133a8e1bf5279e09c7b21
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-01-17 22:01:55 +0200

    Create copies3.java

---
+++ Functional-1/copies3.java
@@ -0,0 +1,17 @@
+/**
+ * Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+ *
+ * @param strings the input list of strings
+ * @return the modified list where each string is replaced by 3 concatenated copies
+ */
+
+public List<String> copies3(List<String> strings) {
+    strings.replaceAll(i -> i + i + i);
+    return strings;
+}
+
+public List<String> copies3(List<String> strings) {
+    return strings.stream()
+            .map(i -> i + i + i)
+            .collect(Collectors.toList());
+}
