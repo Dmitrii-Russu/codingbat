f89bbb3c7b70549522100ac0f7567f61ac30f554
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-01-18 07:44:36 +0200

    Create rightDigit.java

---
+++ Functional-1/rigftDigit.java
@@ -0,0 +1,17 @@
+/**
+ * Given a list of non-negative integers, return a list of the rightmost digits of each number.
+ *
+ * @param nums the input list of non-negative integers
+ * @return the modified list containing the rightmost digits of each number
+ */
+
+public List<Integer> rightDigit(List<Integer> nums) {
+    nums.replaceAll(i -> i % 10);
+    return nums;
+}
+
+public List<Integer> rightDigit(List<Integer> nums) {
+    return nums.stream()
+            .map(i -> i % 10)
+            .collect(Collectors.toList());
+}
