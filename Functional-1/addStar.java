ae37150fdc96ce08863e6933778cf79c638cf6f5
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-01-22 22:16:52 +0200

    Create addStar.java

---
+++ Functional-1/addStar.java
@@ -0,0 +1,15 @@
+/**
+ * Given a list of strings, return a list where each string has "*" added at its end.
+ *
+ * @param strings the input list of strings
+ * @return the modified list where "*" is added to the end of each string
+ */
+
+public List<String> addStar(List<String> strings) {
+    strings.replaceAll(i -> i + "*");
+    return strings;
+}
+
+public List<String> addStar(List<String> strings) {
+    return strings.stream().map(i -> i + "*").collect(Collectors.toList());
+}
