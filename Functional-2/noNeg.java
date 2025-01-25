60363caf092d081a6553522b9be9f5df90c89d2c
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-01-25 16:54:12 +0200

    Update noNeg.java

--- Functional-2/noNeg.java
+++ Functional-2/noNeg.java
@@ -1,1 +1,15 @@
+/**
+ * Given a list of integers, return a list of the integers, omitting any that are less than 0.
+ *
+ * @param nums the input list of integers
+ * @return the modified list with integers >= 0
+ */
 
+public List<Integer> noNeg(List<Integer> nums) {
+    return nums.stream().filter(i -> i >= 0).toList();
+}
+
+public List<Integer> noNeg(List<Integer> nums) {
+    nums.removeIf(i -> i < 0);
+    return nums;
+}
