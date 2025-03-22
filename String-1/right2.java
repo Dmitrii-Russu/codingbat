12419626dd8538bb3a577311bf3541fafd3ec0fe
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-03-22 06:52:17 +0200

    Update right2.java

--- String-1/right2.java
+++ String-1/right2.java
@@ -1,1 +1,11 @@
+/**
+ * Given a string, return a "rotated right 2" version where the last 2 chars 
+ * are moved to the start. The string length will be at least 2.
+ *
+ * @param str the input string
+ * @return the rotated string with the last 2 characters moved to the front
+ */
 
+public String right2(String str) {
+    return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+}
