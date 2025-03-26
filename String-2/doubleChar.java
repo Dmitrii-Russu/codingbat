bded132cf9ab9fbd4cae1961633ac40e327a78be
Dmitrii-Russu <dmitriirussulink@gmail.com>
2025-03-26 15:23:25 +0200

    Update doubleChar.java

--- String-2/doubleChar.java
+++ String-2/doubleChar.java
@@ -6,14 +6,22 @@
  */
 
 public String doubleChar(String str) {
-   
-    return Arrays.stream(str.split("")) 
-            .map(s -> s + s) 
-            .collect(Collectors.joining()); 
+  
+  int index = 0;
+  String[] chars = str.split("");
+  String[] result = new String[chars.length * 2];
+  for(int i = 0; i < chars.length; i++) {
+    result[index++] = chars[i];
+    result[index++] = chars[i];
+  }
+  return String.join("", result);
 }
 
 public String doubleChar(String str) {
-  
+    return Arrays.stream(str.split("")).map(s -> s + s).collect(Collectors.joining()); 
+}
+
+public String doubleChar(String str) {
     String result = "";
     for (int i = 0; i < str.length(); i++) {
         result = result + str.charAt(i) + str.charAt(i);
@@ -22,10 +30,19 @@ public String doubleChar(String str) {
 }
   
 public String doubleChar(String str) {    
-  
   String result = "";
   for (int i = 0; i < str.length(); i++) {
       result = result + str.substring(i, i + 1) + str.substring(i, i + 1);
     }
     return result;
+}
+
+public String doubleChar(String str) {  
+   String[] chars = str.split("");
+   String[] result = new String[chars.length * 2];
+   for(int i = 0; i < chars.length; i++) {
+    result[i * 2] = chars[i];
+    result[i * 2 + 1] = chars[i];
+  }
+  return String.join("", result);
 }
